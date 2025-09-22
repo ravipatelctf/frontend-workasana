6676a8199b38d70d3f7bfe72ea4c94b7dc8d5015
ravipatelctf <ravipatelctf@gmail.com>
2025-09-22 07:17:43 +0530

    chore:add logo to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,6 +23,7 @@ export default function Sidebar({data}) {
     return (
         <div className="container d-flex flex-column gap-4 w-100 py-3">
             <div className="d-flex flex-column gap-3">
+                <div className="fw-bold fs-2 d-none d-md-block" style={{color: "#8D5F8C"}}>Workasana</div>
                 <Link to={`/${data?.email}`} className="fw-bold mb-1 text-secondary text-decoration-none d-flex align-items-center gap-1">
                     <MdOutlineDashboard className="fs-4 text-secondary" />
                     <span className="fw-bold fs-5 text-secondary">Dashboard</span>
