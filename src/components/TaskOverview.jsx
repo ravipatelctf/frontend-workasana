683869d8c9d98c09f17df3af5104eda5e212e554
Ravi Shankar Patel <ravipatelctf@gmail.com>
2025-09-24 18:25:37 +0530

    feat: filter tasks by status

--- src/components/TaskOverview.jsx
+++ src/components/TaskOverview.jsx
@@ -1,8 +1,41 @@
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { baseUrl } from "../api";
 import { useFetchGet } from "../useFetchGet";
 import { Link } from "react-router-dom";
+
+
+
+function FilterTasksByStatus({tasks, setFilteredTasks, filterByStatus, setFilterByStatus}) {
+    
+
+    useEffect(() => {
+        if (filterByStatus) {
+            const result = tasks?.filter((task) => task.status === filterByStatus);
+            setFilteredTasks(result)
+        } else {
+            setFilteredTasks(tasks)
+        }
+
+    }, [filterByStatus, tasks, setFilteredTasks]);
+    
+
+    return (
+        <select 
+            id="filterByStatus"
+            value={filterByStatus}
+            className="form-select"
+            onChange={(e) => setFilterByStatus(e.target.value)}
+        >
+            <option value="" disabled>Filter By status:</option>
+            <option value="To Do">To Do</option>
+            <option value="In Progress">In Progress</option>
+            <option value="Completed">Completed</option>
+            <option value="Blocked">Blocked</option>   
+        </select>
+    );
+}
+
 
 
 function TaskCard({task}) {
@@ -24,15 +57,33 @@ export function TaskOverview({referesh, handleShowAddNewTask}) {
 
     const {data: tasks, fetchData} = useFetchGet(`${baseUrl}/tasks`);
     
+    const [filterByStatus, setFilterByStatus] = useState("");
+    const [filteredTasks, setFilteredTasks] = useState([]);
+
+    
     useEffect(() => {
         fetchData();
     }, [referesh]);
+
+
+    useEffect(() => {
+        if (tasks) {
+            setFilteredTasks(tasks);
+        }
+    }, [tasks]);
+
+    function handleClearFilter() {
+        setFilteredTasks([]);
+        setFilterByStatus("");
+    }
     
     return (
         <section className="mb-5">
             <div className="d-flex align-items-center justify-content-between">
-                <div className="d-flex align-items-center gap-1 mb-2">
+                <div className="d-flex align-items-center justify-content-start gap-1 mb-2">
                     <h3 className="m-0 fs-2 fw-bold">Tasks</h3>
+                    {tasks && <FilterTasksByStatus tasks={tasks} setFilteredTasks={setFilteredTasks} filterByStatus={filterByStatus} setFilterByStatus={setFilterByStatus} />}
+                    <button className="btn btn-outline-dark btn-sm fw-bold" onClick={handleClearFilter}>Clear</button>
                 </div>
                 <button 
                     onClick={() => handleShowAddNewTask(true)} 
@@ -42,7 +93,7 @@ export function TaskOverview({referesh, handleShowAddNewTask}) {
             </div>
             <div className="row mt-3">
                 {
-                    tasks ? ( tasks.map((task) => (
+                    tasks ? ( filteredTasks?.map((task) => (
                         <TaskCard key={task._id} task={task} />
                     ))
                     ) : (
